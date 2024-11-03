ff883fd63907a18d9c98bdd5c7b69bdc0b324a30
Yuri Pierri <91505118+YuriPierriV@users.noreply.github.com>
2024-11-03 20:24:56 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_BACKEND_URL || "http://localhost:5000");
+const socket = io("https://flaskconcurrency.onrender.com");
 
 
 const Square = () => {
